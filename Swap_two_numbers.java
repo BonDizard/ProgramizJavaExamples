287c58206f109c3bb5039f7774b5be970b47d2c7
Bharath Kumar s <114819425+BonDizard@users.noreply.github.com>
2023-02-13 23:47:52 +0530

    Create Swap_two_numbers.java

---
+++ Swap_two_numbers.java
@@ -0,0 +1,22 @@
+package IISC;
+
+import java.util.Scanner;
+
+public class Swap_two_numbers {
+
+	public static void main(String[] args) {
+		float firstnum;
+		float secondnum;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the first number: ");
+		firstnum = scan.nextFloat();
+		System.out.println("Enter the second number: ");
+		secondnum = scan.nextFloat();
+		
+		System.out.println("The first number is " + secondnum);
+		System.out.println("The Second number is : " +firstnum);
+		
+	}
+
+}
+
