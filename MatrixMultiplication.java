5d65e08c0a50cebb46c27d74b07640c1a64c6b29
Bharath Kumar s <114819425+BonDizard@users.noreply.github.com>
2023-02-14 00:01:47 +0530

    Create MatrixMultiplication.java

---
+++ MatrixMultiplication.java
@@ -0,0 +1,18 @@
+package IISC;
+
+public class MatrixMultiplication {
+
+	public static void main(String[] args) {
+		int[][] fire = { { 2, 2 }, { 3, 3 } };
+		int[][] water = { { 1, 1 }, { 4, 4 } };
+		int k[][] = { { 1, 2 }, { 3, 4 } };
+		for (int i = 0; i <= 1; i++) {
+			for (int j = 0; j <= 1; j++) {
+				k[i][j] = fire[i][j] + water[i][j];
+				System.out.print(k[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
