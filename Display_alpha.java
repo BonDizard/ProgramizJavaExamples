2936f768a2be2ffc38468c26425886d55460ef8e
Bharath Kumar s <114819425+BonDizard@users.noreply.github.com>
2023-02-13 23:52:49 +0530

    Create Display_alpha.java

---
+++ Display_alpha.java
@@ -0,0 +1,10 @@
+package IISC;
+
+public class Display_alpha {
+public static void main(String[] args) {
+	for(int i = 1; i<=26;i++) {
+		char u = (char) (i +64); 
+		System.out.println(u);
+	}
+}
+}
