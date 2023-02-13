e8c88ef9f7970b1ff10f083c605bb4286f3e39cf
Bharath Kumar s <114819425+BonDizard@users.noreply.github.com>
2023-02-13 23:46:03 +0530

    Create Print_Integer.java

---
+++ Print_Integer.java
@@ -0,0 +1,15 @@
+package IISC;
+
+import java.util.Scanner;
+
+public class Print_Integer {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number yopu want to print: ");
+	    int num = scan.nextInt();
+	    System.out.println("The number you want to print is:"+num);
+
+	}
+
+}
