e7963ebb01d6dfcba6c5b6e3660d9a92a1957d03
Bharath Kumar s <114819425+BonDizard@users.noreply.github.com>
2023-02-13 23:54:34 +0530

    Create Prime_number.java

---
+++ Prime_number.java
@@ -0,0 +1,23 @@
+package IISC;
+
+import java.util.Scanner;
+
+public class Prime_number {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int num = scan.nextInt();
+		boolean flag = false;
+		for (int i = 2; i <= num / 2; ++i) {
+			if (num % i == 0) {
+				flag = true;
+				break;
+			}
+		}
+		if (!flag)
+			System.out.println(num + " is a prime number.");
+		else
+			System.out.println(num + " is not a prime number.");
+	}
+}
+
